c69a50c65bdc80f66869a6724fa509cd36edddac
Carlos Daniel <144396126+carlosdaniel-20@users.noreply.github.com>
2024-10-16 21:06:20 -0300

    atualizei a imagem do menu
    Quando clilcar na imagem do menu também abrirá o menu de categorias de jogos

--- catalogo_jogos/catalogo/script.js
+++ catalogo_jogos/catalogo/script.js
@@ -1,10 +1,17 @@
 const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
 
+// evento de clique no texto menu para abrir o menu
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
 
+const menuImage = document.getElementById("menuimg");
+
+// Evento de clique na imagem para abrir o menu
+menuImage.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
 
 const categoriasButton = document.getElementById("botao-categorias");
 const categoriasLista = document.getElementById("categorias-lista");
